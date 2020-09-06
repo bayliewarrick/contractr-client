e3facbad62fba987de1d30e5a70b9561cd823364
unknown <bayliewarrick2019@gmail.com>
2020-09-05 20:48:23 -0700

    cleaned some stuff up and created a login component. todo: seperate login and signup components into a navigation container, redirect to login on signup, redirect to profile page(todo:create profile page lol) on login

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Signup from './components/Signup'
-import Appbar from './components/common/appbar'
+import Signup from './components/Signup';
+import Login from './components/Login';
+
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Appbar />
       <Signup />
       <StatusBar style="auto" />
     </View>
---
+++ components/Login/index.js
@@ -0,0 +1,95 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable import/no-unresolved */
+import { View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
+
+import axios from 'axios';
+
+import React, { Component } from '../../node_modules/react';
+
+import styles from './styles';
+import commonStyles from '../common/commonStyles'
+
+class logIn extends Component {
+
+  state = {
+      email: '', password: ''
+  }
+
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val })
+  }
+
+  logIn = async () => {
+   const { email, password } = this.state
+
+ 
+
+    const res = await axios.post('http://10.0.0.202:3001/user/login', {email, password}).then(function (response) {
+      
+      alert('Logged in:' + response.data.userObject.email)
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+
+    
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View style={commonStyles.headerContent}>
+
+        <Text style={commonStyles.headerText}>
+          Log in
+        </Text>
+
+        <Text style={commonStyles.headerSubText}>
+          Enter your account info to log in:
+        </Text>
+
+        </View>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={val => this.onChangeText('email', val)}
+        />
+
+        <TextInput
+          secureTextEntry
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={val => this.onChangeText('password', val)}
+        />
+
+<View style={commonStyles.buttonContainer}>
+
+<TouchableOpacity
+          style={commonStyles.button}
+          onPress={this.logIn}
+        ><Text style={commonStyles.buttonText}>Log In</Text></TouchableOpacity>
+
+        
+<TouchableOpacity
+        style={commonStyles.textLink}
+
+          onPress={this.logIn}
+        ><Text style={commonStyles.buttonText}>Forgot Password? click to reset</Text></TouchableOpacity>
+
+</View>
+
+</View>
+
+    );
+  }
+}
+
+logIn.propTypes = {
+
+};
+
+
+export default logIn;
